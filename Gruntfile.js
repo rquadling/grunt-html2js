b0ef5af0fe11a8ff1e18d4f540c30a9a00668473
Pascal Precht <pascal.precht@googlemail.com>
2013-12-03 11:50:40 +0100

    chore(grunt): adds load-grunt-tasks module
    makes it easier to maintain grunt plugins that have to be loaded

--- Gruntfile.js
+++ Gruntfile.js
@@ -198,9 +198,7 @@ module.exports = function(grunt) {
   grunt.loadTasks('tasks');
 
   // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+  require('load-grunt-tasks')(grunt);
 
   // Whenever the "test" task is run, first clean the "tmp" dir, then run this
   // plugin's task(s), then test the result.
