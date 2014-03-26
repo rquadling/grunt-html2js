f55e34f862c5819f57cd4c6736e8bc936d2f9297
Heide <katze.katze@yahoo.de>
2014-03-26 17:30:42 +0100

    added ability to replace the base value with a different string. Use replace_base_with option in Gruntfile to specify the replace text

--- tasks/html2js.js
+++ tasks/html2js.js
@@ -117,7 +117,13 @@ module.exports = function(grunt) {
 
       var modules = f.src.filter(existsFilter).map(function(filepath) {
 
-        var moduleName = normalizePath(path.relative(options.base, filepath));
+        var moduleName;
+        if (options.replace_base_with === undefined) {
+          moduleName = normalizePath(path.relative(options.base, filepath));
+        } else {
+          moduleName = filepath.replace(options.base, options.replace_base_with);
+        }
+
         if (grunt.util.kindOf(options.rename) === 'function') {
           moduleName = options.rename(moduleName);
         }
