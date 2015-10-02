d261b693fd8939607a6d43d5ba74ab4280161a27
Bruzzese, Frank <Frank.Bruzzese@Kronos.com>
2015-10-02 11:53:30 -0400

    Fixes #59 and #56.  Added options to support AMD modules.

---
+++ test/expected/amd_module_custom_suffix.js
@@ -0,0 +1,12 @@
+define(['angular'], function(angular){angular.module('templates-amd_module_custom_suffix', ['../test/fixtures/one.tpl.html', '../test/fixtures/two.tpl.html']);
+
+angular.module("../test/fixtures/one.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/one.tpl.html",
+    "1 2 3");
+}]);
+
+angular.module("../test/fixtures/two.tpl.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/two.tpl.html",
+    "Testing");
+}]);
+}); //Custom!
