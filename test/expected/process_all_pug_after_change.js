ee077ec5a6189864e9cb20291fc426558358b2e0
Benjamin Norwood <benjamin.norwood@cgifederal.com>
2016-11-09 15:29:27 -0600

    #83 Add pug rendering capabilities and unit tests (major release package.json update reflects the addition). Alphabetized dependency list and added pugg as an optional dependency.

---
+++ test/expected/process_all_pug_after_change.js
@@ -0,0 +1,21 @@
+angular.module('templates-process_all_pug', ['../test/fixtures/process_pug.pug', '../test/fixtures/process_pug_custom.pug', '../test/fixtures/process_pug_with_include.pug', '../test/fixtures/pug_include.pug']);
+
+angular.module("../test/fixtures/process_pug.pug", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/process_pug.pug",
+    "<p class=\"example\">Hello World!</p><div id=\"greeting\">Nice</div><div id=\"watch\">test</div>");
+}]);
+
+angular.module("../test/fixtures/process_pug_custom.pug", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/process_pug_custom.pug",
+    "<a href=\"href\">Great</a>");
+}]);
+
+angular.module("../test/fixtures/process_pug_with_include.pug", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/process_pug_with_include.pug",
+    "<h1>I'm an include!</h1>");
+}]);
+
+angular.module("../test/fixtures/pug_include.pug", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/pug_include.pug",
+    "<h1>I'm an include!</h1>");
+}]);
