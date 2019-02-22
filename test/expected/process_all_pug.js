ba859467d6725e3d48d2411e8591fa25f09362e1
Richard Quadling <Richard.Quadling@DigiTickets.co.uk>
2019-02-22 13:23:03 +0000

    Upgrade dependencies to latest versions (with a small fix for the file ordering when npm audit currently shows no 0 vulnerabilities

--- test/expected/process_all_pug.js
+++ test/expected/process_all_pug.js
@@ -1,9 +1,4 @@
-angular.module("templates-process_all_pug", ["../test/fixtures/process_pug.pug", "../test/fixtures/process_pug_custom.pug", "../test/fixtures/process_pug_with_include.pug", "../test/fixtures/pug_include.pug"]);
-
-angular.module("../test/fixtures/process_pug.pug", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("../test/fixtures/process_pug.pug",
-    "<p class=\"example\">Hello World!</p><div id=\"greeting\">Nice</div><div id=\"watch\">test</div>");
-}]);
+angular.module("templates-process_all_pug", ["../test/fixtures/process_pug_custom.pug", "../test/fixtures/process_pug_with_include.pug", "../test/fixtures/process_pug.pug", "../test/fixtures/pug_include.pug"]);
 
 angular.module("../test/fixtures/process_pug_custom.pug", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("../test/fixtures/process_pug_custom.pug",
@@ -13,6 +8,11 @@ angular.module("../test/fixtures/process_pug_custom.pug", []).run(["$templateCac
 angular.module("../test/fixtures/process_pug_with_include.pug", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("../test/fixtures/process_pug_with_include.pug",
     "<h1>I'm an include!</h1>");
+}]);
+
+angular.module("../test/fixtures/process_pug.pug", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("../test/fixtures/process_pug.pug",
+    "<p class=\"example\">Hello World!</p><div id=\"greeting\">Nice</div><div id=\"watch\">test</div>");
 }]);
 
 angular.module("../test/fixtures/pug_include.pug", []).run(["$templateCache", function($templateCache) {
